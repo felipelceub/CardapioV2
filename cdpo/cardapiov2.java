df0db718ff002b27e5b95192258d20fcd3d39a37
felipelceub <felipe.trevisol@sempreceub.com>
2024-10-09 10:49:41 -0300

    Update cardapiov2.java

--- cdpo/cardapiov2.java
+++ cdpo/cardapiov2.java
@@ -17,6 +17,7 @@ public class Cardapio{
 	static List<String> listaNascimentoClientes = new ArrayList<>();
 	static List<String> listaTelefoneClientes = new ArrayList<>();
 	static List<String> listaEnderecoClientes = new ArrayList<>();
+	static List<Boolean> listaAutorizacaoClientes = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -38,7 +39,7 @@ public class Cardapio{
 		
 	}
 	
-	public static void getAllInfo() {
+	public static void getAllProductInfo() {
 		
 		System.out.println("-------------------------------------------------------------------------------------------------");
         System.out.println("CÓDIGO PRODUTO                                                                              VALOR");
@@ -61,7 +62,7 @@ public class Cardapio{
 		System.out.println("Digite 3 para editar um produto.");
 		System.out.println("Digite 4 para remover um produto.");
 		System.out.println("Digite 5 para rever o menu.");
-		System.out.println("Digite 6 para cadastrar um cliente.");
+		System.out.println("Digite 6 para ver o menu de serviços de cliente.");
 		System.out.println("Digite 7 para finalizar o processo");
 	}
 	
@@ -72,7 +73,7 @@ public class Cardapio{
 			break;
 		
 		case 2:
-			getAllInfo();
+			getAllProductInfo();
 			break;
 		
 		case 3:
@@ -88,6 +89,41 @@ public class Cardapio{
 			break;
 		
 		case 6:
+			System.out.println("Digite 1 para cadastrar um novo cliente.");
+			System.out.println("Digite 2 para ver todos os clientes.");
+			System.out.println("Digite 3 para editar um cliente.");
+			System.out.println("Digite 4 para remover um cliente.");
+			System.out.println("Digite 5 para voltar ao menu.");
+			int operator = sc.nextInt();
+				menuClientes(operator, sc);
+			break;
+		default:
+			System.out.println("digito incorreto...");
+			break;
+		}
+
+			
+	}
+	
+	public static void menuClientes(int op, Scanner sc) {
+		switch(op) {
+		case 1:
+			cadastrarCliente(sc);
+			break;
+		
+		case 2:
+			getAllClientInfo();
+			break;
+		
+		case 3:
+			//TODO editarCliente(sc);
+			break;
+		
+		case 4:
+			removerCliente(sc);
+			break;
+		
+		case 5:
 			break;
 		default:
 			System.out.println("digito incorreto...");
@@ -323,6 +359,190 @@ public class Cardapio{
 		}while(dataNasc.length() != 10);
 		
 		listaNascimentoClientes.add(dataNasc);
+		
+		int autorizacao;
+		do {
+			System.out.println("O cliente deseja estar na lista de mensagens? 1 para sim, 2 para nao");
+				autorizacao = sc.nextInt();
+				if(autorizacao == 1) {
+					listaAutorizacaoClientes.add(true);
+				}
+				else if(autorizacao == 2) {
+					listaAutorizacaoClientes.add(false);
+				}
+				else {
+					
+				}
+			
+		} while(autorizacao != 1 || autorizacao != 2);
+		
+	}
+	
+	
+	public static void removerCliente(Scanner sc) {
+		System.out.print("Digite o index do produto a ser excluido: ");
+			int index = sc.nextInt();
+		
+		listaNomeClientes.remove(index);
+		listaEnderecoClientes.remove(index);
+		listaTelefoneClientes.remove(index);
+		listaSexoClientes.remove(index);
+		listaAutorizacaoClientes.remove(index);
+		listaEmailClientes.remove(index);
+		listaSexoClientes.remove(index);
+		
+		System.out.println("removido com sucesso!");
+	}
+	
+	
+	public static void getAllClientInfo() {
+		for(int i = 0; i < listaNomeClientes.size(); i++) {
+			System.out.println("index do cliente: " + i + "," + listaNomeClientes.get(i) + ", " + listaEnderecoClientes.get(i)+ ", " + listaTelefoneClientes.get(i)
+			+ ", " + listaSexoClientes.get(i) + ", " + listaEmailClientes.get(i) + ", " + listaAutorizacaoClientes.get(i));
+		}
+	}
+	
+	
+	public static void editarCliente(int index, Scanner sc) {
+		sc.nextLine();
+		
+		String nome, sexo, telefone, email, dataNasc;
+		String endereco, enderecoA = null, enderecoB = null;
+		byte tamanhoDasVariaveis;
+		
+		do {
+			System.out.println("Nome do cliente: ");
+			nome = sc.nextLine();
+			
+		} while(nome.length() < 6 || nome.length() > 60); 
+		
+		
+		do {
+			System.out.println("Sexo (F para feminino | M para masculino): ");
+				sexo = sc.nextLine();
+					sexo.toUpperCase();
+					
+		} while(sexo != "F" || sexo != "M");
+		
+		listaSexoClientes.add(index, sexo);
+		
+		do {
+			System.out.println("Logradouro: ");
+			endereco = sc.nextLine();
+		
+		} while(endereco.length() > 60);
+		
+		do {
+			System.out.println("Numero: ");
+				String numero = sc.nextLine();
+			tamanhoDasVariaveis = (byte) numero.length();
+			
+			if(numero != null) {
+				enderecoA = endereco.concat(", " + numero + ", ");
+				
+			}
+			
+			
+		} while(tamanhoDasVariaveis > 4);
+		
+		do {
+			System.out.println("Complemento: ");
+				String complemento = sc.nextLine();
+			tamanhoDasVariaveis = (byte) complemento.length();
+			
+			if(complemento != null) {
+				enderecoB = enderecoA.concat(complemento + ", ");
+
+			}
+		
+		} while(tamanhoDasVariaveis > 60);
+		
+		enderecoA = null;
+		
+		do {
+			System.out.println("Bairro: ");
+				String bairro = sc.nextLine();
+			tamanhoDasVariaveis = (byte) bairro.length();
+			enderecoA = enderecoB.concat(bairro + ", ");
+		
+		} while(tamanhoDasVariaveis > 60 || tamanhoDasVariaveis <= 0);
+		
+		enderecoB = null;
+		
+		do {
+			System.out.println("Cidade: ");
+				String cidade = sc.nextLine();
+			tamanhoDasVariaveis = (byte) cidade.length();
+			enderecoB = enderecoA.concat(cidade + ", ");
+		
+		} while(tamanhoDasVariaveis > 60 || tamanhoDasVariaveis <= 0);
+		
+		enderecoA = null;
+		
+		do {
+			System.out.println("CEP: ");
+				String cep = sc.nextLine();
+			tamanhoDasVariaveis = (byte) cep.length();
+			enderecoA = enderecoB.concat(cep + ", ");
+		
+		} while(tamanhoDasVariaveis != 8);
+		
+		enderecoB = null;
+		
+		do {
+			System.out.println("Estado: ");
+				String estado = sc.nextLine();
+			tamanhoDasVariaveis = (byte) estado.length();
+			enderecoB = enderecoA.concat(estado);
+		
+		} while(tamanhoDasVariaveis != 2);
+		
+				
+		do {
+			System.out.println("telefone: ");
+				telefone = sc.nextLine();
+			tamanhoDasVariaveis = (byte) telefone.length();
+		
+		} while(tamanhoDasVariaveis != 11);
+				
+		do {
+			System.out.println("Email: ");
+				email = sc.nextLine();
+			tamanhoDasVariaveis = (byte) email.length();
+		} while(tamanhoDasVariaveis > 80);
+		
+		do {
+			System.out.println("Data de nascimento (dd/MM/yyyy): ");
+			dataNasc = sc.nextLine();
+		}while(dataNasc.length() != 10);
+		
+		listaNascimentoClientes.add(dataNasc);
+		
+		int autorizacao;
+		do {
+			System.out.println("O cliente deseja estar na lista de mensagens? 1 para sim, 2 para nao");
+				autorizacao = sc.nextInt();
+			
+		} while(autorizacao != 1 || autorizacao != 2);
+		
+		listaNomeClientes.add(index, nome);
+		listaSexoClientes.add(index, sexo);
+		listaEnderecoClientes.add(enderecoB);
+		listaTelefoneClientes.add(telefone);
+		if(email.length() == 0) {
+			listaEmailClientes.add(null);
+		}
+		else {
+		listaEmailClientes.add(index, email);
+		}
+		listaNascimentoClientes.add(index, dataNasc);
+		if(autorizacao == 1) {
+			listaAutorizacaoClientes.add(true);
+		}
+		else if(autorizacao == 2) {
+			listaAutorizacaoClientes.add(false);
+		}
+
 		
 	}
 	
